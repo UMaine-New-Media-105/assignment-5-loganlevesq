5134426bb8521d88783dd94e5a60a912be7bbce0
loganlevesq <124173740+loganlevesq@users.noreply.github.com>
2023-04-10 12:53:21 -0400

    Create a5q1.js

---
+++ a5q1.js
@@ -0,0 +1,49 @@
+let bubbles = [];
+
+function setup() {
+  createCanvas(960, 540);
+  for (let i = 0; i < 5; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(20, 50);
+    bubbles.push(new Bubble(x, y, r));
+  }
+}
+
+function draw() {
+  background(220);
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].move();
+    bubbles[i].show();
+  }
+}
+
+class Bubble {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.xSpeed = random(-5, 5);
+    this.ySpeed = random(-5, 5);
+  }
+  
+  move() {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    
+    if (this.x < 0 || this.x > width) {
+      this.xSpeed *= -1;
+    }
+    if (this.y < 0 || this.y > height) {
+      this.ySpeed *= -1;
+    }
+  }
+  
+  show() {
+    push();
+    translate(this.x, this.y);
+    fill(255, 200);
+    ellipse(0, 0, this.r * 2);
+    pop();
+  }
+}
